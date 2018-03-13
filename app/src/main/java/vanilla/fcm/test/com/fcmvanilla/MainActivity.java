38e443191d45c22f59b7ddd7c4885f27d4bba219
leochien <leochien@google.com>
2018-03-13 14:11:48 +0800

    init commit

---
+++ app/src/main/java/vanilla/fcm/test/com/fcmvanilla/MainActivity.java
@@ -0,0 +1,13 @@
+package vanilla.fcm.test.com.fcmvanilla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}

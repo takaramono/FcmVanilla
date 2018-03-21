ea20eebf7736ec6724328eb73cda8b6a0a9ef55f
leochien(Mac) <leochien@google.com>
2018-03-21 14:45:44 +0800

    I don't pretty sure... add mylibrary.jar anyway

--- app/src/main/java/vanilla/fcm/test/com/fcmvanilla/MainActivity.java
+++ app/src/main/java/vanilla/fcm/test/com/fcmvanilla/MainActivity.java
@@ -2,6 +2,9 @@ package vanilla.fcm.test.com.fcmvanilla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.mylibrary.TinyHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Log.d("Main", "3 + 4 = " + TinyHelper.add(3,4));
   }
 }
---
+++ mylibrary/src/main/java/com/example/mylibrary/TinyHelper.java
@@ -0,0 +1,9 @@
+package com.example.mylibrary;
+
+/**
+ * Created by leochien on 3/21/18.
+ */
+
+public class TinyHelper {
+  public static int add(int a, int b) {return a+b;}
+}
